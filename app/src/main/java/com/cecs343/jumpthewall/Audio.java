533c8873e1769398446d0a1d9404c5d3221a0671
BryanDiNardo <Diablo@Bryans-MacBook-Pro.local>
2016-11-30 18:15:08 -0800

    Scaled main menu background

---
+++ app/src/main/java/com/cecs343/jumpthewall/Audio.java
@@ -0,0 +1,23 @@
+package com.cecs343.jumpthewall;
+
+/**
+ * Created by Diablo on 11/30/16.
+ */
+import android.media.AudioManager;
+
+public class Audio
+{
+    
+
+    public boolean currentMusicState;
+
+    public void adjustVolume(int direction, int flags)
+    {
+
+    }
+
+    public boolean isMusicActive()
+    {
+        return currentMusicState;
+    }
+}
